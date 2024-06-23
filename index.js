78e427ed387d285895b6ea32ca37609968639073
Thiennn262626 <nguyendinhthien10a5@gmail.com>
2024-06-23 14:00:56 +0700

    port 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 app.use(cors());
 
-const port = 3001;
+const port = 3000;
 
 const { connectToDatabase } = require("./config");
 connectToDatabase();
