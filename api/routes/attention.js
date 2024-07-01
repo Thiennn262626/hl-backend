bf151eebc75cb7b9cb785997d28ac43c6128d718
Thiennn262626 <nguyendinhthien10a5@gmail.com>
2024-07-01 19:02:17 +0700

    reset server

--- api/routes/attention.js
+++ api/routes/attention.js
@@ -27,7 +27,7 @@ router.post(
       } else {
         await setProductAttention(user_id, product_id);
         await RedisService.setJson(key, true);
-        await RedisService.expire(key, 180);
+        await RedisService.expire(key, 300);
 
         return response.status(200).json({
           message: "success",
@@ -58,4 +58,105 @@ async function setProductAttention(user_id, product_id) {
   }
 }
 
-//input: id san pham va khoang thoi gian trong ngay
+// router.get("/generate-attentions", async (request, response) => {
+//   try {
+//     const products = await getProducts();
+//     const users = await getUsers();
+//     let a = [];
+//     let index = 0;
+//     for (const product of products) {
+//       let rs = {};
+//       let b = [];
+//       rs.createdDate = product.createdDate;
+//       const userCount = getRandomInt(3, 20);
+//       const selectedUsers = getRandomElements(users, userCount);
+//       for (const user of selectedUsers) {
+//         const randomDate = getRandomDate(
+//           new Date(product.createdDate),
+//           getDateAfter(15)
+//         );
+//         b.push(randomDate);
+//         await setProductAttention(user.user_id, product.product_id, randomDate);
+//         console.log(
+//           user.user_id,
+//           product.product_id,
+//           randomDate,
+//           getDateAfter(15),
+//           index++
+//         );
+//       }
+//       rs.attention = b.length;
+//       a.push(rs);
+//     }
+
+//     response.status(200).json({
+//       message: "success",
+//       total: a.length,
+//       a: a,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     response.status(500).json({
+//       error: "Internal Server Error",
+//     });
+//   }
+// });
+
+// async function getProducts() {
+//   const query = "SELECT id as product_id, createdDate FROM Product";
+//   const result = await new sql.Request().query(query);
+//   return result.recordset;
+// }
+
+// async function getUsers() {
+//   const query = "SELECT id as user_id FROM [User]";
+//   const result = await new sql.Request().query(query);
+//   return result.recordset;
+// }
+
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// function getRandomElements(array, count) {
+//   const shuffled = array.slice(0);
+//   let i = array.length;
+//   const min = i - count;
+//   let temp, index;
+
+//   while (i-- > min) {
+//     index = Math.floor((i + 1) * Math.random());
+//     temp = shuffled[index];
+//     shuffled[index] = shuffled[i];
+//     shuffled[i] = temp;
+//   }
+
+//   return shuffled.slice(min);
+// }
+
+// function getRandomDate(start, end) {
+//   return new Date(
+//     start.getTime() + Math.random() * (end.getTime() - start.getTime())
+//   );
+// }
+// function getDateAfter(days) {
+//   const today = new Date();
+//   const futureDate = new Date(today);
+//   futureDate.setDate(today.getDate() + days); // Thêm số ngày cần tính (15 ngày trong trường hợp này)
+//   return futureDate;
+// }
+
+// async function setProductAttention(user_id, product_id, createdDate) {
+//   try {
+//     // const query = `INSERT INTO Attention (user_id, product_id, createdDate) VALUES (@user_id, @product_id, @createdDate)`;
+//     const query = `INSERT INTO Subcribe (id_user, idProduct, createdDate) VALUES (@user_id, @product_id, @createdDate)`;
+//     await new sql.Request()
+//       .input("user_id", user_id)
+//       .input("product_id", product_id)
+//       .input("createdDate", createdDate)
+//       .query(query);
+//   } catch (error) {
+//     console.log(error);
+//     throw "not found product";
+//   }
+// }
